a0d1a6dcf488ec806d5333286a57a89e17a16cca
adelinetang <adelinetang@cpp.edu>
2022-03-21 01:42:38 -0700

    stack changes
    Linked and Resizablestack changes. One test method add in calculator test

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,5 +1,7 @@
 public class CalculatorTest {
-    public static void main(Sting[] args){
-
+    public static void main(String[] args){
+        ResizableArrayStack<Integer> Stack = new ResizableArrayStack<>();
+        String result1 = Stack.evaluatePostfix("53-2*6+");
+        System.out.println(result1);
     }
 }
--- LinkedStack.java
+++ LinkedStack.java
@@ -1,8 +1,84 @@
-import java.util;
-public class LinkedStack <T> implements StackInterface <T>{
+import java.util.EmptyStackException;
+public class LinkedStack<T> implements StackInterface<T>{
+    private Node topNode; // References the first node in the chain
+  
+    public LinkedStack()
+    {
+       topNode = null;
+    } // end default constructor
+ 
+     private class Node
+     {
+       private T    data; // Entry in stack
+       private Node next; // Link to next node
+       
+       private Node(T dataPortion)
+       {
+          this(dataPortion, null);
+       } // end constructor
+       
+       private Node(T dataPortion, Node linkPortion)
+       {
+          data = dataPortion;
+          next = linkPortion;
+       } // end constructor
+       
+       private T getData()
+       {
+          return data;
+       } // end getData
+       
+       private void setData(T newData)
+       {
+          data = newData;
+       } // end setData
+       
+       private Node getNextNode()
+       {
+          return next;
+       } // end getNextNode
+       
+       private void setNextNode(Node nextNode)
+       {
+          next = nextNode;
+       } // end setNextNode
+     } // end Node
+     
+    public void push(T newEntry)
+    {
+      Node newNode = new Node(newEntry, topNode);
+      topNode = newNode;
+      //topNode = new Node(newEntry, topNode); // Alternate code
+    } // end push
 
-    //Algorithm
-    String convertToPostfix(infix){
+    public T pop()
+    {
+    T top = peek();  // Might throw EmptyStackException
+    // Assertion: topNode != null
+    topNode = topNode.getNextNode();
+    return top;
+    } // end pop
+
+    public boolean isEmpty()
+    {
+   return topNode == null;
+    } // end isEmpty
+
+    public void clear()
+    {
+   topNode = null;
+    } // end clear
+
+    public T peek()
+    {
+   if (isEmpty())
+      throw new EmptyStackException();
+   else
+      return topNode.getData();
+    } // end peek
+
+
+   public String convertToPostfix(String infix){
         Stack <char> operatorStack = new Stack <char> ();
         String postfix = new String();
         for(char nextCharacter : infix){
